10fc4436a00bce36a2073f6854752e4f11ec496c
hata <hata@tinypulse.com>
2017-06-26 07:03:24 +0700

    Read detail message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,9 +11,37 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.sender = current_user
+    if @message.save
+      flash[:success] = 'Sent message successful'
+      redirect_to messages_path
+    else
+      flash[:error] = "#{@message.errors.full_messages.to_sentence}"
+      redirect_to 'new'
+    end
   end
 
   def sent
-    @messages = Message.order(:created_at).where(sender_id: current_user.id)
+    @messages = Message.order(:updated_at).where(sender_id: current_user.id)
+  end
+
+  def show
+    @message = Message.find(params[:id])
+    if @message.is_read?
+      render 'read'
+    elsif current_user.id != @message.sender.id
+      if current_user.id == @message.recipient.id
+        @message.is_read = true
+        !@message.save
+      else
+        redirect_to messages_path
+      end
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:subject, :body, :recipient_id)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :recipient, class_name: 'User', foreign_key: 'recipient_id'
+  validates :subject, presence: true, length: { minimum: 1, maximum: 255 }
 end
